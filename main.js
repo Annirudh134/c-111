79c45fd37a86ec59c2805006baaa7168c3f0c70d
Annirudh134 <81172126+Annirudh134@users.noreply.github.com>
2021-10-11 16:53:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+Webcam.set({
+    width:360,
+    height:300,
+    image_format: 'png' ,
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot() 
+{
+    Webcam.snap(function(data_url) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_url+'"/>';
+    });
+}
+
+console.log('m15 version:', m15.version);
+
+classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json',modelLoaded);
+
+function modelLoaded() (
+    console.log('Model Loaded!'):
+)
